5bae25bb2d416bb2695271d810499f60ee2cad1f
Buñuelotuenifai <93409334+matixx25@users.noreply.github.com>
2022-04-11 23:42:38 -0500

    Login/register

--- Login/index.php
+++ Login/index.php
@@ -1,28 +1,17 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>myreview login</title>
+	<title>Myreview</title>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/index.css">
 </head>
 <body>
-	<form action="validar.php" method="post"> <!--form es para formulario "action: es donde vamos a mandar la informacion" "method:Es como vamos a mandar la informacion"    -->
 
-	<h1>Myreview</h1>
-
-	<input type="text" placeholder="Usuario" name="usuario"><br><br>
-	<!-- ponemos un input y le decimos que es de tipo texto,el place holder es para que en el input se muestre un ejemplo de lo que poner y name seria el nombre del input-->
-	<input type="password" placeholder="Contraseña" name="conraseña"><br><br>
-
-	
-	<input type="submit" name="ingresar">
-		
-
-
-
-
-
-
-	</form>  
+	<?php require 'partials/header.php'?>
+	<h1>¡Un lugar fantastico!</h1>
+	<a href="Login.php">Login</a> or
+	<a href="Singup.php">Sing up</a>
 </body>
 </html>
