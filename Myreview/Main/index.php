5f153ce7668fdc183b13ce7a7dd0108c63b27116
Buñuelotuenifai <93409334+matixx25@users.noreply.github.com>
2022-04-20 21:27:06 -0500

    Add files via upload

--- Myreview/Main/index.php
+++ Myreview/Main/index.php
@@ -25,16 +25,18 @@
 		<section class="menu_container">
 
 
-			<a href="index.php"><h1 class="menu_logo">Myreview</h1></a>
+			<h1 class="menu_logo">Myreview</h1>
 
 			
-			  <form class="container-fluid">
+			  		<form class="container">
 				    <div class="input-group">
-				      
+				    	
 				      <input type="text" class="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1">
 				      <button class="input-group-text" id="basic-addon1"><img src="assets/search.svg" class=""></button>
+				  		
 				   </div>
-			  </form>
+				   </form>
+			  	
 			
 
 
@@ -123,16 +125,20 @@
 				} 
 			?>
 
-
+			<!---
 			<?php
+
 				if (isset($_SESSION['auth'])) {
-					if ($_SESSION['auth']) {	
+					if ($_SESSION['auth']) {
+						print('Hola, '.$_SESSION['name']);
+						print('<br><br><br><a href="../Login/Logout.php">Logout</a>');
 					} else {
 						print('<a href="../Login/Login.php">Login</a> or
 						<a href="../Login/Singup.php">Sing up</a>');
 					}		
 				}
 			?>
+			--->
 		</section>
 
 		
@@ -142,35 +148,28 @@
 
 
 
-
+	
 
 	
 <!-- Start WOWSlider.com BODY section -->
 <div id="wowslider-container1">
 <div class="ws_images"><ul>
-		<li><img src="data1/images/140_auto_x2.jpg" alt="140_auto_x2" title="140_auto_x2" id="wows1_0"/></li>
-		<li><img src="data1/images/realismomagico_auto_x2.jpg" alt="realismo-magico_auto_x2" title="realismo-magico_auto_x2" id="wows1_1"/></li>
-		<li><img src="data1/images/librosdeterror1.jpg" alt="libros-de-terror-1" title="libros-de-terror-1" id="wows1_2"/></li>
-		<li><a href="http://wowslider.net"><img src="data1/images/lasmejoressagasdefantasiaconcluidas1024x819_auto_x2.jpg" alt="slider" title="lasmejoressagasdefantasiaconcluidas1024x819_auto_x2" id="wows1_3"/></a></li>
-		<li><img src="data1/images/cthulu_auto_x2.jpg" alt="cthulu_auto_x2" title="cthulu_auto_x2" id="wows1_4"/></li>
+		<li><img src="data1/images/fantasa.png" alt="Fantasía" title="Fantasía" id="wows1_0"/></li>
+		<li><img src="data1/images/futurismo.png" alt="Futurismo" title="Futurismo" id="wows1_1"/></li>
+		<li><a href="http://wowslider.net"><img src="data1/images/terror.png" alt="slider html" title="Terror" id="wows1_2"/></a></li>
+		<li><img src="data1/images/realismo_magico.png" alt="Realismo Magico" title="Realismo Magico" id="wows1_3"/></li>
 	</ul></div>
 	<div class="ws_bullets"><div>
-		<a href="#" title="140_auto_x2"><span><img src="data1/tooltips/140_auto_x2.jpg" alt="140_auto_x2"/>1</span></a>
-		<a href="#" title="realismo-magico_auto_x2"><span><img src="data1/tooltips/realismomagico_auto_x2.jpg" alt="realismo-magico_auto_x2"/>2</span></a>
-		<a href="#" title="libros-de-terror-1"><span><img src="data1/tooltips/librosdeterror1.jpg" alt="libros-de-terror-1"/>3</span></a>
-		<a href="#" title="lasmejoressagasdefantasiaconcluidas1024x819_auto_x2"><span><img src="data1/tooltips/lasmejoressagasdefantasiaconcluidas1024x819_auto_x2.jpg" alt="lasmejoressagasdefantasiaconcluidas1024x819_auto_x2"/>4</span></a>
-		<a href="#" title="cthulu_auto_x2"><span><img src="data1/tooltips/cthulu_auto_x2.jpg" alt="cthulu_auto_x2"/>5</span></a>
-	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">slider</a> by WOWSlider.com v9.0</div>
+		<a href="#" title="Fantasía"><span><img src="data1/tooltips/fantasa.png" alt="Fantasía"/>1</span></a>
+		<a href="#" title="Futurismo"><span><img src="data1/tooltips/futurismo.png" alt="Futurismo"/>2</span></a>
+		<a href="#" title="Terror"><span><img src="data1/tooltips/terror.png" alt="Terror"/>3</span></a>
+		<a href="#" title="Realismo Magico"><span><img src="data1/tooltips/realismo_magico.png" alt="Realismo Magico"/>4</span></a>
+	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">javascript photo gallery</a> by WOWSlider.com v9.0</div>
 <div class="ws_shadow"></div>
 </div>	
 <script type="text/javascript" src="engine1/wowslider.js"></script>
 <script type="text/javascript" src="engine1/script.js"></script>
 <!-- End WOWSlider.com BODY section -->
-	<a href="addbook.php "><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAALpJREFUSEvdldENwjAMRF8nYITCJoxCJ4NRGAU2YAOQJSKlEb3YVisQ+Y3vLj5bl4GNz7AxP0rgCJyBffARN2ACroZTAlY4BslLuWEPPYHnuzpq4wynwF8VUOKrdJASaEFukmYRFjvoCZT7pcUq8/xdgfrlbvvqNe1Z9F8CKkHSQ/bGklvgAey8rE3dvaSwGrLF9SWRqEZ++hTX2XCTTaoOku7MYbVA1nP5X9SXWc/dAqtY0pJEv8PwI16SVTgZvIpLeQAAAABJRU5ErkJggg=="/></a>
-	
-
-
-
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <p>	hola</p>	
 
