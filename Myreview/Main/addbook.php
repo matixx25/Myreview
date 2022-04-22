2fe7dddbf90b0f269aa03a453cdb067f23f8b8bd
jero020 <102270277+jero020@users.noreply.github.com>
2022-04-21 20:29:08 -0500

    Add files via upload

---
+++ Myreview/Main/addbook.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- End WOWSlider.com -->
+	<meta charset="utf-8">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Myreview</title>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="assets/css/index.css">
+	<link href="https://fonts.googleapis.com/css2?family=PT+Sans:wgth@400;700&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://kit.fontawesome.com/712575d4a5.js" crossorigin="anonymous" ></script>
+	<!-- Start WOWSlider.com HEAD section -->
+	<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+	<script type="text/javascript" src="engine1/jquery.js"></script>
+	<!-- End WOWSlider.com HEAD section -->
+
+</head>
+<body>
+	
+
+	
+	<nav class="menu">
+		<section class="menu_container">
+
+
+			<a href="index.php"><h1 class="menu_logo">Myreview</h1></a>
+
+			
+			  <form class="container-fluid">
+				    <div class="input-group">
+				      
+				      <input type="text" class="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1">
+				      <button class="input-group-text" id="basic-addon1"><img src="assets/search.svg" class=""></button>
+				   </div>
+			  </form>
+			
+
+
+
+			<ul class="menu_links">
+
+				<li class="menu_item">
+					<a class="menu_link">Home</a>
+				</li>
+
+
+
+				<li class="menu_item menu_item--show">
+					<a class="menu_link">Perfil <img src="assets/arrow.svg" class="menu_arrow"></a>
+					<ul class="menu_nesting">
+
+						<li class="menu_inside">
+							<a class="menu_link menu_link--inside">F</a>
+						</li>
+
+						<li class="menu_inside">
+							<a class="menu_link menu_link--inside">T</a>
+						</li>
+
+						<li class="menu_inside">
+							<a class="menu_link menu_link--inside">C</a>
+						</li>
+
+						<li class="menu_inside">
+							<a class="menu_link menu_link--inside">P</a>
+						</li>
+					</ul>
+				</li>
+
+
+					<li class="menu_item menu_item--show">
+						<a class="menu_link">About<img src="assets/arrow.svg" class="menu_arrow"></a>
+						<ul class="menu_nesting">
+
+							<li class="menu_inside">
+								<a class="menu_link menu_link--inside">About 1</a>
+							</li>
+
+							<li class="menu_inside">
+								<a class="menu_link menu_link--inside">About 2</a>
+							</li>
+
+							<li class="menu_inside">
+								<a class="menu_link menu_link--inside">About 3</a>
+							</li>
+
+							<li class="menu_inside">
+								<a class="menu_link menu_link--inside">About 4</a>
+							</li>
+						</ul>
+					</li>
+
+				<li class="menu_item">
+					<a class="menu_link">Contact</a>
+				</li>
+
+				<li class="menu_item menu_item--show">
+					<img src="assets/img/perfil_default.png" class="menu_item menu_item--show" id="picture_profile">
+					<ul class="menu_profile">
+
+						<li class="menu_inside">
+							<p class="menu_link menu_link--inside">Cantidad de reseñas</p>
+							<img src="assets/img/perfil_default.png" id="picture_profile2" >
+						</li>
+
+					</ul>
+
+
+				</li>
+
+			</ul>
+
+			<div class="menu_hamburger">
+				
+				<img src="assets/menu.svg" class="menu_img">
+			</div>
+			<br>
+		</section>
+	</nav>
+
+
+	<div id="opinion">
+		<center><input type="text" name="$Opinion"></center>
+
+
+
+		</div>
+
+<?php
+
+
+
+?>
+
+
+</body>
+</html>
